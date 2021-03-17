3cc5c9eac0f00daeceaab9d2e9c2e7de040adc81
Arthur <parabellum357@hotmail.com>
2021-03-17 11:14:47 +0500

    hw4 [Саитгалин]

---
+++ hw4/script.py
@@ -0,0 +1,28 @@
+import os
+from typing import List
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('forks', type=int, help='number of forks')
+parser.add_argument('iterations', type=int, help='number of iterations')
+parser.add_argument('message', type=str, help='message')
+
+args = parser.parse_args()
+
+child_pids: List[int] = []
+
+for i in range(1, args.forks + 1):
+    pid = os.fork()
+    if (pid == 0):
+        child_pids.append(pid)
+    else:
+        for j in range(args.iterations * i):
+            print(f'fork = {i}\titeration = {j}\tmessage = {args.message}')
+        exit(0)
+        
+for pid in child_pids:
+    try:
+        os.waitpid(pid, 0)
+    except ChildProcessError:
+        continue
